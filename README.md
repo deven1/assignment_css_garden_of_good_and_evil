b54ab01459f60dc41cde93df7126a5eecef03a7c
Egle Libby <eglelibby@gmail.com>
2017-04-03 12:35:41 -0400

    basic file structure and radio button

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # assignment_css_garden_of_good_and_evil
 Create a CSS garden that adapts to your whims.
+ Egle Libby
---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+
+// Set up form body parsing
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set up cookies
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// Set up handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("index");
+})
+
+// Listen on port 3000
+app.listen(3000, () => {
+  console.log("Listening!");
+});
